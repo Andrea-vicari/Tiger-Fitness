b8ac5d4de98e2b90d9160164d787342dfc160915
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-04-01 14:58:58 +0200

    Update ElencoComponentiPage.jsx

--- client/src/pages/dashpages/ElencoComponentiPage.jsx
+++ client/src/pages/dashpages/ElencoComponentiPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
 import Navbar from '../../Components/Common/DashNav';
-import ListaComponenti from '../../Components/dashcomponents/ListaComponenti';
+import ListaComponenti from '../../Components/dashcomponents/ListaUtenti';
 import ScaricaListaCompCSV from '../../Components/dashcomponents/ScaricaListaCompCSV';
 
 function ElencoComponentiPage() {
   return (
     <>
     <Navbar />
-    <ListaComponenti />
+    <ListaUtenti />
     <Footer />
     </>
   )
